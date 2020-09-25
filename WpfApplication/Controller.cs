40bbc32dddd939ad14b2f431136eb1bccfcfd371
al.tsayun <al.tsayun@gmail.com>
2020-09-25 13:00:19 +0300

    Mostly complete Edit panel interface

---
+++ WpfApplication/Controller.cs
@@ -0,0 +1,7 @@
+namespace WpfApplication
+{
+    public class Controller
+    {
+        
+    }
+}
--- WpfApplication/MainWindow.xaml.cs
+++ WpfApplication/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace WpfApplication
+﻿using System;
+using System.Windows;
+
+namespace WpfApplication
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -8,6 +11,38 @@
         public MainWindow()
         {
             InitializeComponent();
+        }
+        
+
+        private void Control_OnOpenFileClick(object sender, EventArgs e)
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        private void resetRotation(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void resetBrightness(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void resetContrast(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void resetScale(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Control_OnSaveFileClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
